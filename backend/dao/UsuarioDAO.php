2869d0b6e50fc433ed8e7b92f97a668ea91fa087
BrunoArrudas <brunoguilhermechaveiro@gmail.com>
2024-05-03 21:53:33 -0300

    Usuario DAO Create

--- backend/dao/UsuarioDAO.php
+++ backend/dao/UsuarioDAO.php
@@ -1,106 +1,113 @@
 <?php
 
-require_once '../config/Database.php';
-require_once '../entity/Usuario.php';
+require_once 'config/Database.php';
+require_once 'entity/Usuario.php';
+require_once 'BaseDAO.php';
 
 class UsuarioDAO implements BaseDAO {
     private $db;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->db = Database::getInstance();
     }
 
-    public function getById($id){
-        
+    public function getById($id) {
         try {
+            // Preparar a consulta SQL
+            $sql = "SELECT * FROM Usuario WHERE Id = :id";
 
-        //Preparar a consulta SQL
-        $sql = "SELECT * FROM Usuario WHERE Id = :id"; //: Sintaxe do PDO
-
-        //Preparar a instrução
-        $stmt = $this->db->prepare($sql);
-
-        //Vincular parâmetros
-        $stmt->bindParam(':id', $id);
-        
-        //Executa a instrução
-        $stmt->execute();
-        
-        //Obtem o usuario encontrado;
-        $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        //Retorna o usuario encontrado
-        return $usuario ? 
-        new Usuario(
-
-        $usuario['Id'],
-        $usuario['NomeUsuario'], 
-        $usuario['Senha'],
-        $usuario['Email'], 
-        $usuario['GrupoUsuarioID'],
-        $usuario['Ativo'], 
-        $usuario['DataCriacao'],
-        $usuario['DataAtualizacao'] 
-        
-        ) :null;
-
-    } catch (PDOException $e){
-        return null;
-    }
-
-}
-
-//-----------------------------------------------------//
-
-    public function getAll(){
-        try {
-
-            //Preparar a consulta SQL
-            $sql = "SELECT * FROM Usuario"; 
-    
-            //Preparar a instrução
+            // Preparar a instrução
             $stmt = $this->db->prepare($sql);
-            
-            //Executa a instrução
+
+            // Vincular parâmetros
+            $stmt->bindParam(':id', $id);
+
+            // Executa a instrução
             $stmt->execute();
-            
-            //Obtem o usuario encontrado;
-            $usuarios = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            return array_map(function ($usuario) {
-                return  new Usuario(
-    
-                $usuario['Id'],
-                $usuario['NomeUsuario'], 
-                $usuario['Senha'],
-                $usuario['Email'], 
-                $usuario['GrupoUsuarioID'],
-                $usuario['Ativo'], 
-                $usuario['DataCriacao'],
-                $usuario['DataAtualizacao']);
+            // Obtem o usuario encontrado;
+            $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            }, $usuarios);
-    
-        } catch (PDOException $e){
+            // Retorna o usuário encontrado
+            return $usuario ? 
+                new Usuario($usuario['Id'],
+                            $usuario['NomeUsuario'], 
+                            $usuario['Senha'], 
+                            $usuario['Email'], 
+                            $usuario['GrupoUsuarioID'],
+                            $usuario['Ativo'],
+                            $usuario['DataCriacao'],
+                            $usuario['DataAtualizacao']) 
+                : null;
+        } catch (PDOException $e) {
             return null;
         }
     }
 
-//-----------------------------------------------------//  
+    public function getAll() {
+        try {
+            // Preparar a consulta SQL
+            $sql = "SELECT * FROM Usuario";
 
-    public function create($entity){
+            // Preparar a instrução
+            $stmt = $this->db->prepare($sql);
+
+            // Executa a instrução
+            $stmt->execute();
+
+            // Obtem o usuario encontrado;
+            $usuarios = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            return array_map(function ($usuario) {
+                return new Usuario($usuario['Id'],
+                    $usuario['NomeUsuario'], 
+                    $usuario['Senha'], 
+                    $usuario['Email'], 
+                    $usuario['GrupoUsuarioID'],
+                    $usuario['Ativo'],
+                    $usuario['DataCriacao'],
+                    $usuario['DataAtualizacao']);
+            }, $usuarios);            
+        } catch (PDOException $e) {
+            return null;
+        }
+    }
+
+    public function create($usuario) {
+        try {
+            // Preparar a consulta SQL
+            $sql = "INSERT INTO Usuario( NomeUsuario , Senha , Email , GrupoUsuarioID , Ativo , DataCriacao , DataAtualizacao , UsuarioAtualizacao )
+                    VALUES(:nomeUsuario, :senha, :email, :grupoUsuarioID, :ativo, current_timestamp(),current_timestamp(),null)";
+            
+            // Preparar a instrução
+            $stmt = $this->db->prepare($sql);
+
+            //Vincular parâmetros
+            $stmt->bindParam(':nomeUsuario', $usuario->getNomeUsuario());
+            $stmt->bindParam(':Senha', $usuario->getNomeSenha());
+            $stmt->bindParam(':Email', $usuario->getNomeEmail());
+            $stmt->bindParam(':GrupoUsuarioID', $usuario->getGrupoUsuarioID());
+            $stmt->bindParam(':Ativo', $usuario->getAtivo());
+
+            //Executar a instrução
+            $stmt->execute();
+
+            //Retornar verdadeiro se a inserção for bem sucedida
+            return true;
+
+        } catch (PDOException $e) {
+            // TO-DO: implementar log
+            return false;
+        }
+    }
+
+    public function update($usuario) {
 
     }
 
-    public function update($entity){
+    public function delete($id) {
 
     }
-
-    public function delete($id){
-
-    }
-
 }
 
 ?>
--- backend/index.php
+++ backend/index.php
@@ -1,17 +1,16 @@
 <?php
-     
     require_once("template/header.php");
-     
+    require_once 'dao/UsuarioDAO.php';
+
+    $usuarioDao = new UsuarioDAO();
+    $usuario = $usuarioDao->getById(1);
+    $x = $usuario->getSenha();
+    echo "$x";
+
 ?>
-
     <h1>Olá Sistema Vendas Body</h1>
-
-    
 </body>
 
 <?php
-
     require_once("template/footer.php");
-
 ?>
- 
